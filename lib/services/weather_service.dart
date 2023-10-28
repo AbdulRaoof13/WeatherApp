f51f737bbe1e524d339fcc679eff1312d64a9c66
AbdulRaoof13 <a.rauf13bughio@gmail.com>
2023-10-29 01:34:31 +0500

    model and service created

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,21 @@
+import '../models/weather_models.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WeatherService {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
