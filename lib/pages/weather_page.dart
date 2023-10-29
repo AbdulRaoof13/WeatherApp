16cceb4997a25f4b7dc426f101ed81e0d16baa53
AbdulRaoof13 <a.rauf13bughio@gmail.com>
2023-10-30 01:37:13 +0500

    made some progress

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,10 @@
+import "dart:ffi";
+
 import "package:flutter/material.dart";
+import "package:lottie/lottie.dart";
+import "package:weather_app/models/weather_models.dart";
+import "package:weather_app/services/weather_service.dart";
+import 'package:intl/intl.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +14,84 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('f9986c35934242e897db3d124820ae68');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  String getAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('kk').format(now);
+
+    bool? Day = true;
+
+    if (int.parse(formattedDate) > 18) {
+      Day = false;
+    }
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloudy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        {
+          if (Day == true) {
+            return 'assets/partly sunny rain.json';
+          } else {
+            return 'assets/partly night rain.json';
+          }
+        }
+      case 'snow':
+        return 'assets/snow.json';
+      default:
+        {
+          if (Day == true) {
+            return 'assets/sunny.json';
+          } else {
+            return 'assets/clear night.json';
+          }
+        }
+    }
+  }
+
   @override
+  void initState() {
+    //implement initState
+    super.initState();
+
+    _fetchWeather();
+  }
+
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_weather?.cityName ?? "Loading..."),
+          Lottie.asset(getAnimation(_weather?.mainCondition ?? "")),
+          Text('${_weather?.temperature.round()}°C'),
+          Text(_weather?.mainCondition ?? "")
+        ],
+      ),
+    ));
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,3 +1,6 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
 import '../models/weather_models.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -17,5 +20,22 @@ class WeatherService {
     } else {
       throw Exception('Failed to load data');
     }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemark[0].locality;
+
+    return city ?? "";
   }
 }
